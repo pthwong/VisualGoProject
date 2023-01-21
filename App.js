be1094f3c2129f98564dc36c8863db599d08da3d
pthwong <tszhim.wong1010@yahoo.com>
2023-01-21 17:46:35 +0800

    Updated

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+// import NavContainer from './NavContainer';
+
+function App() {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+export default App;
