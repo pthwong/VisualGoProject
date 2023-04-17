948de79a6ab1fad2827f77039992bd5cf508da1a
pthwong <tszhim.wong1010@yahoo.com>
2023-04-17 17:10:44 +0800

    App can display the news with between the start date and the end date
    App can display the news with between the start date and the end date with the connection from backend REST API from mysql database in Chinese.

---
+++ backendAPI_code/community_news.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/", function (req, res, next) {
+  connection.query("SELECT * FROM News", function (error, results, fields) {
+    if (error) {
+      res.send({ status: 500, error: error, response: null });
+    } else {
+      // Adjusting the start and end datetime to GMT+8
+      results.forEach(function (result) {
+        var startDateTime = new Date(result.postStartDateTime);
+        var endDateTime = new Date(result.postEndDateTime);
+        startDateTime.setHours(startDateTime.getHours());
+        endDateTime.setHours(endDateTime.getHours());
+        result.postStartDateTime = startDateTime.toISOString();
+        result.postEndDateTime = endDateTime.toISOString();
+      });
+
+      res.send({
+        status: 200,
+        error: null,
+        response: results,
+      });
+    }
+  });
+});
+
+module.exports = router;
--- view/visuallyImpaired/VICommunityPage.js
+++ view/visuallyImpaired/VICommunityPage.js
@@ -1,129 +1,170 @@
-import React from 'react';
-import {useState, useEffect} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  TouchableOpacity,
-  SafeAreaView,
-} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
-import {Calendar} from 'react-native-calendars';
+import {addDays, format, differenceInDays} from 'date-fns';
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {Agenda} from 'react-native-calendars';
+import {LocaleConfig} from 'react-native-calendars';
+
+LocaleConfig.locales['zh'] = {
+  monthNames: [
+    '一月',
+    '二月',
+    '三月',
+    '四月',
+    '五月',
+    '六月',
+    '七月',
+    '八月',
+    '九月',
+    '十月',
+    '十一月',
+    '十二月',
+  ],
+  monthNamesShort: [
+    '一',
+    '二',
+    '三',
+    '四',
+    '五',
+    '六',
+    '七',
+    '八',
+    '九',
+    '十',
+    '十一',
+    '十二',
+  ],
+  dayNames: [
+    '星期日',
+    '星期一',
+    '星期二',
+    '星期三',
+    '星期四',
+    '星期五',
+    '星期六',
+  ],
+  dayNamesShort: ['周日', '周一', '周二', '周三', '周四', '周五', '周六'],
+};
+
+LocaleConfig.defaultLocale = 'zh';
 
 function VICommunityPage() {
-  //   const [email, onChangeText] = useState('');
-  //   const [password, onChangeText] = useState('');
-  const navigation = useNavigation();
-  const [selected, setSelected] = useState('');
-  const [events, setEvents] = useState({});
+  const [items, setItems] = useState({});
 
   useEffect(() => {
-    // Fetch event data from API endpoint
-    fetch('https://api.whomethser.synology.me:3560/visualgo/v1/news/')
-      .then(response => response.json())
-      .then(data => {
-        // Format event data for calendar view
-        console.log(data);
-        const formattedData = {};
-        data.forEach(event => {
-          const date = event.postStartDateTime.split('T')[0];
-          formattedData[date] = formattedData[date] || {marked: true};
-          formattedData[date].dots = formattedData[date].dots || [];
-          formattedData[date].dots.push({
-            key: event.postID,
-            color: 'blue',
-            selectedDotColor: 'blue',
-            marked: true,
-          });
-        });
-        console.log('Data: ', formattedData);
-        setEvents(formattedData);
+    // run once
+
+    const getData = async () => {
+      const response = await fetch(
+        'https://api.whomethser.synology.me:3560/visualgo/v1/news',
+      );
+      const {response: data} = await response.json();
+
+      const mappedData = data.map(post => {
+        // const date = addDays(new Date(), index);
+
+        const startDate = new Date(post.postStartDateTime);
+        const endDate = new Date(post.postEndDateTime);
+
+        const startDateTime = format(startDate, 'yyyy-MM-dd HH:mm');
+        const endDateTime = format(endDate, 'yyyy-MM-dd HH:mm');
+
+        const daysDifference = differenceInDays(endDate, startDate);
+
+        // Create an array of dates that the event occurs on
+        const eventDates = Array.from(
+          {length: daysDifference + 1},
+          (_, index) => {
+            const currentDate = addDays(startDate, index);
+            return format(currentDate, 'yyyy-MM-dd');
+          },
+        );
+
+        return {
+          ...post,
+          // date: format(date, 'yyyy-MM-dd'),
+          startDate: format(startDate, 'yyyy-MM-dd'),
+          endDate: format(endDate, 'yyyy-MM-dd'),
+          startDateTime,
+          endDateTime,
+          title: post.postTitle,
+          description: post.postDescribe,
+          building: post.postBuilding,
+          district: post.districtID,
+          eventDates,
+        };
       });
+
+      const reduced = mappedData.reduce((acc, currentItem) => {
+        const {eventDates} = currentItem;
+
+        eventDates.forEach(date => {
+          if (!acc[date]) {
+            acc[date] = [];
+          }
+
+          acc[date].push(currentItem);
+        });
+
+        return acc;
+      }, {});
+
+      setItems(reduced);
+    };
+
+    getData();
   }, []);
 
+  const renderItem = item => {
+    if (item.title === undefined) {
+      return (
+        <View style={styles.itemContainer}>
+          <Text style={styles.noNewsText}>No news for this day</Text>
+        </View>
+      );
+    }
+
+    return (
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemTitle}>{item.title}</Text>
+        <Text style={styles.itemSubTitle}>{item.description}</Text>
+        <Text>{item.building}</Text>
+        <Text>
+          由 {item.startDateTime} 至 {item.endDateTime}
+        </Text>
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Calendar
-        onDayPress={day => {
-          setSelected(day.dateString);
-          console.log('selected day', day.dateString);
-        }}
-        current={Date().dateString}
-        style={{
-          borderWidth: 1,
-          borderColor: 'gray',
-          height: 350,
-        }}
-        theme={{
-          backgroundColor: '#ffffff',
-          calendarBackground: '#ffffff',
-          textSectionTitleColor: '#b6c1cd',
-          selectedDayBackgroundColor: '#00adf5',
-          selectedDayTextColor: '#ffffff',
-          todayTextColor: '#00adf5',
-          dayTextColor: '#2d4150',
-        }}
-        markedDates={events}
-      />
-    </View>
+    <SafeAreaView style={styles.safe}>
+      <Agenda items={items} renderItem={renderItem} />
+    </SafeAreaView>
   );
 }
+export default VICommunityPage;
 
 const styles = StyleSheet.create({
-  titleChi: {
-    marginTop: '10%',
-    marginLeft: '5%',
-    marginRight: '5%',
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: 'black',
+  safe: {
+    flex: 1,
   },
-  titleEng: {
-    marginLeft: '5%',
-    marginRight: '5%',
-    fontSize: 28,
-    fontWeight: 'bold',
-    color: 'black',
-  },
-  textField: {
-    fontSize: 18,
-    color: 'black',
-    marginBottom: '5%',
-  },
-  inputField: {
-    border: 1,
+  itemTitle: {
     fontSize: 25,
-    marginTop: '20%',
-    marginLeft: '5%',
-    marginRight: '5%',
   },
-  loginBtn: {
-    backgroundColor: '#97F9F9',
-    color: 'black',
-    width: '75%',
-    marginLeft: '11%',
-    padding: '3%',
-    marginTop: '10%',
-    borderRadius: 50,
-    // shadowOpacity: 0.1,
-  },
-  regBtn: {
-    backgroundColor: '#ffd63f',
-    color: 'black',
-    width: '75%',
-    marginLeft: '11%',
-    padding: '3%',
-    marginTop: '10%',
-    borderRadius: 50,
-    // shadowOpacity: 0.1,
-  },
-  btnTxt: {
-    color: 'black',
-    textAlign: 'center',
+  itemSubTitle: {
     fontSize: 20,
-    shadowOpacity: 0.2,
+  },
+  itemContainer: {
+    backgroundColor: 'white',
+    margin: 5,
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  noNewsText: {
+    flex: 1,
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
-
-export default VICommunityPage;
