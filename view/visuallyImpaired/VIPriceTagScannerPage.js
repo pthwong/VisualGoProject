12291fbcd6e78beb4be8698f83595e2ee94ca7d1
pthwong <tszhim.wong1010@yahoo.com>
2023-04-19 17:52:53 +0800

    Successfully upload price tag to MS Azure Custom Vision and return response

--- view/visuallyImpaired/VIPriceTagScannerPage.js
+++ view/visuallyImpaired/VIPriceTagScannerPage.js
@@ -13,6 +13,13 @@ import {
   CameraPermissionStatus,
   useCameraDevices,
 } from 'react-native-vision-camera';
+import {Buffer} from 'buffer';
+import {AZURE_CV_API_URL, AZURE_CV_API_KEY, AZURE_SUBSCRIPTION_ID} from '@env';
+// import {PredictionAPIClient} from '@azure/cognitiveservices-customvision-prediction';
+// import {AuthenticationCredentials} from '@azure/ms-rest-js/es/lib/msRest';
+
+import axios from 'axios';
+import fs from 'react-native-fs';
 
 function VIPriceTagScannerPage() {
   const navigation = useNavigation();
@@ -23,6 +30,14 @@ function VIPriceTagScannerPage() {
 
   const [showCamera, setShowCamera] = useState(true);
   const [imageSource, setImageSource] = useState('');
+  const [prediction, setPrediction] = useState(null);
+
+  //MS Azure
+  const projectId = '12b272e7-4cf6-4a62-9279-9308aaca3e46';
+  const iterationName = 'Iteration4';
+  const predictionKey = '7308a0fa8d364428af85ad5431749bdb';
+  const endpoint = `https://fypcustomvisionprice-prediction.cognitiveservices.azure.com`;
+  const cvEndpoint = `https://fypcustomvisionprice-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/12b272e7-4cf6-4a62-9279-9308aaca3e46/classify/iterations/Iteration4/image`;
 
   useEffect(() => {
     (async () => {
@@ -38,8 +53,137 @@ function VIPriceTagScannerPage() {
       const photo = await camera.current.takePhoto({});
       setImageSource(photo.path);
       setShowCamera(false);
-      console.log('Photo path: \n', photo.path);
+      console.log('Photo path: \n', imageSource);
     }
+  }
+
+  // async function predictImage(imageUri) {
+  //   const imageUrl = `data:image/jpeg;base64,${imageUri}`;
+  //   const imageBuffer = Buffer.from(imageUrl.split(',')[1], 'base64');
+
+  //   // Create a blob from the image buffer
+  //   const blob = new Blob([imageBuffer], {type: 'image/jpeg'});
+
+  //   // Create a form data object and append the blob to it
+  //   const formData = new FormData();
+  //   formData.append('image', blob, 'image.jpg');
+
+  //   // Set the prediction key and content type headers
+  //   const headers = {
+  //     'Prediction-Key': predictionKey,
+  //     'Content-Type': 'application/octet-stream',
+  //   };
+
+  //   // Make the API request using Axios
+  //   const response = await axios.post(
+  //     `https://fypcustomvisionprice-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/12b272e7-4cf6-4a62-9279-9308aaca3e46/classify/iterations/Iteration4/image`,
+  //     //post: `https://fypcustomvisionprice-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/12b272e7-4cf6-4a62-9279-9308aaca3e46/classify/iterations/Iteration4/image`
+  //     formData,
+  //     {
+  //       headers,
+  //     },
+  //   );
+
+  //   // Return the response data
+  //   return response.data;
+  // }
+
+  // const imageToBase64 = async uri => {
+  //   const response = await fetch(uri);
+  //   const blob = await response.blob();
+
+  //   return new Promise((resolve, reject) => {
+  //     const reader = new FileReader();
+  //     reader.onload = () => {
+  //       resolve(reader.result);
+  //     };
+  //     reader.onerror = reject;
+  //     reader.readAsDataURL(blob);
+  //   });
+  // };
+
+  // async function predictImage(imageUri) {
+  //   try {
+  //     const imageBuffer = await fs.readFile(imageUri, 'base64');
+
+  //     // Convert buffer to base64 string
+  //     const base64Image = Buffer.from(imageBuffer, 'binary').toString('base64');
+
+  //     // const requestBody = {
+  //     //   url: imageUrlBase64,
+  //     // };
+
+  //     // const options = {
+  //     //   method: 'POST',
+  //     //   url: `https://fypcustomvisionprice-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/12b272e7-4cf6-4a62-9279-9308aaca3e46/classify/iterations/Iteration4/image`,
+  //     //   headers: {
+  //     //     'Prediction-Key': '7308a0fa8d364428af85ad5431749bdb',
+  //     //     'Content-Type': 'application/octet-stream',
+  //     //   },
+  //     //   data: Buffer.from(requestBody.url, 'base64'),
+  //     // };
+  //     // Upload image to Custom Vision API
+  //     const response = await axios.post(cvEndpoint, base64Image, {
+  //       headers: {
+  //         'Content-Type': 'application/octet-stream',
+  //         'Prediction-Key': '7308a0fa8d364428af85ad5431749bdb',
+  //       },
+  //     });
+
+  //     // const options = {
+  //     //   headers: {
+  //     //     'Prediction-Key': predictionKey,
+  //     //     'Content-Type': 'application/octet-stream',
+  //     //   },
+  //     // };
+
+  //     // const response = await axios.post(cvEndpoint, imageBuffer, options);
+  //     return response;
+  //   } catch (error) {
+  //     console.log('upload image error: \n', error);
+  //     alert('error');
+  //     return null;
+  //   }
+  // }
+
+  async function predictPriceTag(imageUri) {
+    let filename = imageUri.split('/').pop();
+    let match = /\.(\w+)$/.exec(filename);
+    let type = match ? `image/${match[1]}` : `image`;
+    let formData = new FormData();
+    formData.append('photo', {uri: imageUri, name: filename, type});
+
+    // Make the API request using Axios
+    // const headers = {
+    //   'Prediction-Key': predictionKey,
+    //   'Content-Type': 'multipart/form-data',
+    // };
+
+    await axios
+      .post(
+        `https://fypcustomvisionprice-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/12b272e7-4cf6-4a62-9279-9308aaca3e46/classify/iterations/Iteration4/image`,
+        //post: `https://fypcustomvisionprice-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/12b272e7-4cf6-4a62-9279-9308aaca3e46/classify/iterations/Iteration4/image`
+        formData,
+        {
+          headers: {
+            'Prediction-Key': predictionKey,
+            'Content-Type': 'multipart/form-data',
+          },
+        },
+      )
+      .then(res => {
+        console.log(res);
+        return res;
+      })
+      .catch(err => {
+        console.log(err);
+        return err;
+      });
+  }
+
+  async function displayPredictResult() {
+    const response = predictPriceTag(`${imageSource}`);
+    console.log('Response: ', response);
   }
 
   if (cameraDevice && cameraPermission === 'authorized') {
@@ -77,10 +221,16 @@ function VIPriceTagScannerPage() {
           </>
         ) : (
           <>
+            <Text>價錢牌已拍攝</Text>
             <TouchableOpacity
               style={styles.camButton}
               onPress={() => setShowCamera(true)}>
               <Text style={styles.btnTxt}>重新拍攝</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.camButton}
+              onPress={() => displayPredictResult()}>
+              <Text style={styles.btnTxt}>確認</Text>
             </TouchableOpacity>
             {imageSource !== '' ? (
               <Image
@@ -136,7 +286,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.2,
   },
   image: {
-    width: '100%',
+    width: '80%',
     height: 'auto',
     aspectRatio: 9 / 16,
   },
