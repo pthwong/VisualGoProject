50f1ed14f02aa97a778dc2afc77731999f07bac9
pthwong <tszhim.wong1010@yahoo.com>
2023-04-21 22:47:21 +0800

    code updated

--- view/visuallyImpaired/VIPriceTagScannerPage.js
+++ view/visuallyImpaired/VIPriceTagScannerPage.js
@@ -1,12 +1,5 @@
 import React, {useState, useEffect, useRef} from 'react';
-import {
-  Button,
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  Image,
-} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {
   Camera,
@@ -61,12 +54,144 @@ function VIPriceTagScannerPage() {
     setPrediction(null);
   }
 
+  // async function predictPriceTag(imageUri) {
+  //   let filename = imageUri.split('/').pop();
+  //   let match = /\.(\w+)$/.exec(filename);
+  //   let type = match ? `image/${match[1]}` : `image`;
+  //   let priceTagFormData = new FormData();
+  //   priceTagFormData.append('photo', {uri: imageUri, name: filename, type});
+
+  //   //1. Image classification with different kinds of price tags with MS Azure Custom Vision and return the kind label.
+  //   await axios
+  //     .post(cvEndpoint, priceTagFormData, {
+  //       headers: {
+  //         'Prediction-Key': predictionKey,
+  //         //'Content-Type': 'multipart/form-data',
+  //       },
+  //     })
+  //     .then(async res => {
+  //       console.log(
+  //         'Result:',
+  //         res.data.predictions[0].tagName,
+  //         '\nProbability:',
+  //         res.data.predictions[0].probability,
+  //       );
+  //       if (res.data.predictions[0].tagName === 'other') {
+  //         return;
+  //       } else {
+  //         if (
+  //           res.data.predictions[0].tagName === 'parknshopmarked' &&
+  //           res.data.predictions[0].probability * 100 >= 40
+  //         ) {
+  //           alert(
+  //             'parknshopmarked\nProbability: ' +
+  //               res.data.predictions[0].probability * 100,
+  //           );
+  //           setPrediction(res.data.predictions[0].tagName);
+  //           setpriceTagRecognEndpt(
+  //             `https://fypparknshopmarkedv2.cognitiveservices.azure.com/formrecognizer/documentModels/parknshopmarkedformmodelv2_1:analyze?api-version=2022-08-31`,
+  //           );
+  //           setOcpApimSubKey(`ffd66fa152074fbebe5673c91d6eddae`);
+  //         }
+  //         if (
+  //           res.data.predictions[0].tagName === 'parknshopoffer' &&
+  //           res.data.predictions[0].probability * 100 >= 40
+  //         ) {
+  //           alert(
+  //             'parknshopoffer\nProbability: ' +
+  //               res.data.predictions[0].probability * 100,
+  //           );
+  //           setPrediction(res.data.predictions[0].tagName);
+  //           setpriceTagRecognEndpt(
+  //             `https://fypparknshopofferv2.cognitiveservices.azure.com/formrecognizer/documentModels/parknshopofferformmodelv2_1_new:analyze?api-version=2022-08-31`,
+  //           );
+  //           setOcpApimSubKey(`00d31480815b4113854aeda80222daba`);
+  //         }
+  //         if (
+  //           res.data.predictions[0].tagName === 'wellcomemarked' &&
+  //           res.data.predictions[0].probability * 100 >= 40
+  //         ) {
+  //           alert(
+  //             'wellcomemarked\nProbability: ' +
+  //               res.data.predictions[0].probability * 100,
+  //           );
+  //           setPrediction(res.data.predictions[0].tagName);
+  //           setpriceTagRecognEndpt(
+  //             `https://fypwellcomemarked2.cognitiveservices.azure.com/formrecognizer/documentModels/wellcomemarkedformmodelv2_1:analyze?api-version=2022-08-31`,
+  //           );
+  //           setOcpApimSubKey(`f0ab5363478845fdb8c657dc7b9bef57`);
+  //         }
+  //         if (
+  //           res.data.predictions[0].tagName === 'wellcomeoffer' &&
+  //           res.data.predictions[0].probability * 100 >= 40
+  //         ) {
+  //           alert(
+  //             'wellcomeoffer\nProbability: ' +
+  //               res.data.predictions[0].probability * 100,
+  //           );
+  //           setPrediction(res.data.predictions[0].tagName);
+  //           setpriceTagRecognEndpt(
+  //             `https://fypwellcomemarked2.cognitiveservices.azure.com/formrecognizer/documentModels/wellcomeofferformmodel2_1:analyze?api-version=2022-08-31`,
+  //           );
+  //           setOcpApimSubKey(`1c6126a584bc4e6393d2c46d350be72b`);
+  //         }
+  //       }
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //       alert('Error');
+  //       return err;
+  //     });
+
+  //   //2. Get Price Tag Info URL by Form recognizer
+  //   await axios
+  //     .post(priceTagRecognEndpt, priceTagFormData, {
+  //       headers: {
+  //         'Ocp-Apim-Subscription-Key': ocpApimSubKey,
+  //         //'Content-Type': 'multipart/form-data',
+  //       },
+  //     })
+  //     .then(async res => {
+  //       console.log(
+  //         'Price tag recognition URL:\n ',
+  //         res.headers['operation-location'],
+  //       );
+  //       setPriceTagInfoURL(res.headers['operation-location']);
+  //       console.log('priceTagInfoURL:\n', priceTagInfoURL);
+  //     })
+  //     .catch(err => {
+  //       alert('Error');
+  //       console.log(`2. error: \n`, err);
+  //     });
+
+  //   //3. Get the price tag info by the URL
+  //   await axios
+  //     .post(priceTagInfoURL, priceTagFormData, {
+  //       headers: {
+  //         'Ocp-Apim-Subscription-Key': ocpApimSubKey,
+  //         // 'Content-Type': 'multipart/form-data',
+  //       },
+  //     })
+  //     .then(async res => {
+  //       alert('successful');
+  //       console.log('Result: \n', res.analyzeResult);
+  //     })
+  //     .catch(err => {
+  //       alert('Error');
+  //       console.log(`3. error: \n`, err);
+  //     });
+  // }
+
   async function predictPriceTag(imageUri) {
     let filename = imageUri.split('/').pop();
     let match = /\.(\w+)$/.exec(filename);
     let type = match ? `image/${match[1]}` : `image`;
     let priceTagFormData = new FormData();
     priceTagFormData.append('photo', {uri: imageUri, name: filename, type});
+
+    let formRecogEndpt = '';
+    let formRecogKey = '';
+    let formRegResEndpt = '';
 
     //1. Image classification with different kinds of price tags with MS Azure Custom Vision and return the kind label.
     await axios
@@ -86,61 +211,209 @@ function VIPriceTagScannerPage() {
         if (res.data.predictions[0].tagName === 'other') {
           return;
         } else {
+          //1.1 parknshopmarked
           if (
             res.data.predictions[0].tagName === 'parknshopmarked' &&
             res.data.predictions[0].probability * 100 >= 40
           ) {
+            formRecogEndpt =
+              'https://fypparknshopmarkedv2.cognitiveservices.azure.com/formrecognizer/documentModels/parknshopmarkedformmodelv2_1:analyze?api-version=2022-08-31';
+            formRecogKey = 'ffd66fa152074fbebe5673c91d6eddae';
             alert(
               'parknshopmarked\nProbability: ' +
                 res.data.predictions[0].probability * 100,
             );
             setPrediction(res.data.predictions[0].tagName);
-            setpriceTagRecognEndpt(
-              `https://fypparknshopmarkedv2.cognitiveservices.azure.com/formrecognizer/documentModels/parknshopmarkedformmodelv2_1:analyze?api-version=2022-08-31`,
-            );
-            setOcpApimSubKey(`ffd66fa152074fbebe5673c91d6eddae`);
+            //1.1.1 Get Price Tag Info URL by Form recognizer
+            await axios
+              .post(formRecogEndpt, priceTagFormData, {
+                headers: {
+                  'Ocp-Apim-Subscription-Key': formRecogKey,
+                  //'Content-Type': 'multipart/form-data',
+                },
+              })
+              .then(async res => {
+                console.log(
+                  'Price tag recognition URL:\n ',
+                  res.headers['operation-location'],
+                );
+                formRegResEndpt = res.headers['operation-location'];
+                console.log('priceTagInfoURL:\n', formRegResEndpt);
+              })
+              .catch(err => {
+                alert('Error');
+                console.log(`2. error: \n`, err);
+              });
+
+            //1.1.2 Get the price tag info by the URL
+            await axios
+              .get(formRegResEndpt, {
+                headers: {
+                  'Ocp-Apim-Subscription-Key': formRecogKey,
+                },
+              })
+              .then(result => {
+                alert('successful');
+                console.log('3. Result: \n', result);
+              })
+              .catch(err => {
+                alert('Error');
+                console.log(`3. error: \n`, err);
+              });
+            //1.1.3 got info completed if no error
           }
+          //1.2 parknshopoffer
           if (
             res.data.predictions[0].tagName === 'parknshopoffer' &&
             res.data.predictions[0].probability * 100 >= 40
           ) {
+            formRecogEndpt =
+              'https://fypparknshopofferv2.cognitiveservices.azure.com/formrecognizer/documentModels/parknshopofferformmodelv2_1_new:analyze?api-version=2022-08-31';
+            formRecogKey = '00d31480815b4113854aeda80222daba';
             alert(
               'parknshopoffer\nProbability: ' +
                 res.data.predictions[0].probability * 100,
             );
             setPrediction(res.data.predictions[0].tagName);
-            setpriceTagRecognEndpt(
-              `https://fypparknshopofferv2.cognitiveservices.azure.com/formrecognizer/documentModels/parknshopofferformmodelv2_1_new:analyze?api-version=2022-08-31`,
-            );
-            setOcpApimSubKey(`00d31480815b4113854aeda80222daba`);
+            //1.2.1 Get Price Tag Info URL by Form recognizer
+            await axios
+              .post(formRecogEndpt, priceTagFormData, {
+                headers: {
+                  'Ocp-Apim-Subscription-Key': formRecogKey,
+                  //'Content-Type': 'multipart/form-data',
+                },
+              })
+              .then(async res => {
+                console.log(
+                  'Price tag recognition URL:\n ',
+                  res.headers['operation-location'],
+                );
+                formRegResEndpt = res.headers['operation-location'];
+                console.log('priceTagInfoURL:\n', formRegResEndpt);
+              })
+              .catch(err => {
+                alert('Error');
+                console.log(`2. error: \n`, err);
+              });
+
+            //1.2.2 Get the price tag info by the URL
+            await axios
+              .get(formRegResEndpt, {
+                headers: {
+                  'Ocp-Apim-Subscription-Key': formRecogKey,
+                },
+              })
+              .then(result => {
+                alert('successful');
+                console.log('3. Result: \n', result);
+              })
+              .catch(err => {
+                alert('Error');
+                console.log(`3. error: \n`, err);
+              });
+            //1.2.3 got info completed if no error
           }
+          //1.3 wellcomemarked
           if (
             res.data.predictions[0].tagName === 'wellcomemarked' &&
             res.data.predictions[0].probability * 100 >= 40
           ) {
+            formRecogEndpt =
+              'https://fypwellcomemarked2.cognitiveservices.azure.com/formrecognizer/documentModels/wellcomemarkedformmodelv2_1:analyze?api-version=2022-08-31';
+            formRecogKey = 'f0ab5363478845fdb8c657dc7b9bef57';
             alert(
               'wellcomemarked\nProbability: ' +
                 res.data.predictions[0].probability * 100,
             );
             setPrediction(res.data.predictions[0].tagName);
-            setpriceTagRecognEndpt(
-              `https://fypwellcomemarked2.cognitiveservices.azure.com/formrecognizer/documentModels/wellcomemarkedformmodelv2_1:analyze?api-version=2022-08-31`,
-            );
-            setOcpApimSubKey(`f0ab5363478845fdb8c657dc7b9bef57`);
+            //1.3.1 Get Price Tag Info URL by Form recognizer
+            await axios
+              .post(formRecogEndpt, priceTagFormData, {
+                headers: {
+                  'Ocp-Apim-Subscription-Key': formRecogKey,
+                  //'Content-Type': 'multipart/form-data',
+                },
+              })
+              .then(async res => {
+                console.log(
+                  'Price tag recognition URL:\n ',
+                  res.headers['operation-location'],
+                );
+                formRegResEndpt = res.headers['operation-location'];
+                console.log('priceTagInfoURL:\n', formRegResEndpt);
+              })
+              .catch(err => {
+                alert('Error');
+                console.log(`2. error: \n`, err);
+              });
+
+            //1.3.2 Get the price tag info by the URL
+            await axios
+              .get(formRegResEndpt, {
+                headers: {
+                  'Ocp-Apim-Subscription-Key': formRecogKey,
+                },
+              })
+              .then(result => {
+                alert('successful');
+                console.log('3. Result: \n', result);
+              })
+              .catch(err => {
+                alert('Error');
+                console.log(`3. error: \n`, err);
+              });
+            //1.3.3 got info completed if no error
           }
+          //1.4 wellcomeoffer
           if (
             res.data.predictions[0].tagName === 'wellcomeoffer' &&
             res.data.predictions[0].probability * 100 >= 40
           ) {
+            formRecogEndpt =
+              'https://fypwellcomemarked2.cognitiveservices.azure.com/formrecognizer/documentModels/wellcomeofferformmodel2_1:analyze?api-version=2022-08-31';
+            formRecogKey = '1c6126a584bc4e6393d2c46d350be72b';
             alert(
               'wellcomeoffer\nProbability: ' +
                 res.data.predictions[0].probability * 100,
             );
             setPrediction(res.data.predictions[0].tagName);
-            setpriceTagRecognEndpt(
-              `https://fypwellcomemarked2.cognitiveservices.azure.com/formrecognizer/documentModels/wellcomeofferformmodel2_1:analyze?api-version=2022-08-31`,
-            );
-            setOcpApimSubKey(`1c6126a584bc4e6393d2c46d350be72b`);
+            //1.4.1 Get Price Tag Info URL by Form recognizer
+            await axios
+              .post(formRecogEndpt, priceTagFormData, {
+                headers: {
+                  'Ocp-Apim-Subscription-Key': formRecogKey,
+                  //'Content-Type': 'multipart/form-data',
+                },
+              })
+              .then(async res => {
+                console.log(
+                  'Price tag recognition URL:\n ',
+                  res.headers['operation-location'],
+                );
+                formRegResEndpt = res.headers['operation-location'];
+                console.log('priceTagInfoURL:\n', formRegResEndpt);
+              })
+              .catch(err => {
+                alert('Error');
+                console.log(`2. error: \n`, err);
+              });
+
+            //1.4.2 Get the price tag info by the URL
+            await axios
+              .get(formRegResEndpt, {
+                headers: {
+                  'Ocp-Apim-Subscription-Key': formRecogKey,
+                },
+              })
+              .then(result => {
+                alert('successful');
+                console.log('3. Result: \n', result);
+              })
+              .catch(err => {
+                alert('Error');
+                console.log(`3. error: \n`, err);
+              });
+            //1.4.3 got info completed if no error
           }
         }
       })
@@ -148,44 +421,6 @@ function VIPriceTagScannerPage() {
         console.log(err);
         alert('Error');
         return err;
-      });
-
-    //2. Get Price Tag Info URL by Form recognizer
-    await axios
-      .post(priceTagRecognEndpt, priceTagFormData, {
-        headers: {
-          'Ocp-Apim-Subscription-Key': ocpApimSubKey,
-          //'Content-Type': 'multipart/form-data',
-        },
-      })
-      .then(async res => {
-        console.log(
-          'Price tag recognition URL:\n ',
-          res.headers['operation-location'],
-        );
-        setPriceTagInfoURL(res.headers['operation-location']);
-        console.log('priceTagInfoURL:\n', priceTagInfoURL);
-      })
-      .catch(err => {
-        alert('Error');
-        console.log(`2. error: \n`, err);
-      });
-
-    //3. Get the price tag info by the URL
-    await axios
-      .post(priceTagInfoURL, priceTagFormData, {
-        headers: {
-          'Ocp-Apim-Subscription-Key': ocpApimSubKey,
-          // 'Content-Type': 'multipart/form-data',
-        },
-      })
-      .then(async res => {
-        alert('successful');
-        console.log('Result: \n', res.analyzeResult);
-      })
-      .catch(err => {
-        alert('Error');
-        console.log(`3. error: \n`, err);
       });
   }
 
